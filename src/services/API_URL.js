7a3b0ff2626aada3c7b63302247204719c9bf837
abdom11 <abd123abdo.com@gmail.com>
2024-02-26 00:55:57 +0300

    check api url update

--- src/services/API_URL.js
+++ src/services/API_URL.js
@@ -1,12 +1,6 @@
 import { User, ToastHandler } from "@utils";
 
 const url = process.env.EXPO_PUBLIC_API_URL;
-// const desiredPart = url
-//   .split("https://")[1]
-//   .split(".ngrok-free.app")[0]
-//   .split("-")
-//   .slice(0, 5)
-//   .join("-");
 
 export const getIdCode = () => {
   return url;
