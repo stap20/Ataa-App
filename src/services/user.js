61c2405522a38418f50fc9f0e309cc7f8e7106d3
abdom11 <abd123abdo.com@gmail.com>
2024-02-25 20:53:32 +0300

    test the logic

--- src/services/user.js
+++ src/services/user.js
@@ -20,7 +20,7 @@ export default {
     try {
       const data = { phoneNumber, countryCode, password };
       const response = await API.post("/login", data);
-      console.log(response);
+      ToastHandler.showToast(`Login Error: ${response}`, true);
       if (response.data.success) {
         User.login({ ...response.data.user });
 
