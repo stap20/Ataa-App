1494b38d5d1a78b35c5bb94274b8a25cccdb5fe0
abdom11 <abd123abdo.com@gmail.com>
2024-02-25 22:26:04 +0300

    sssss

--- src/services/API.js
+++ src/services/API.js
@@ -40,8 +40,8 @@ API.interceptors.response.use(
 
 export const testConnection = async () => {
   try {
-    alert(API_URL + "/");
-    const response = await axios.post(API_URL + "/");
+    alert("http://192.168.100.15:3000" + "/");
+    const response = await axios.post("http://192.168.100.15:3000" + "/");
 
     if (!response) {
       alert("ya5taaaaaaaaaaay v1");
