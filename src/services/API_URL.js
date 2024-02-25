74aa5708378071f8cacea9d5a1ae83573d8b2290
abdom11 <abd123abdo.com@gmail.com>
2024-02-25 21:19:41 +0300

    cheka

--- src/services/API_URL.js
+++ src/services/API_URL.js
@@ -12,6 +12,6 @@ export const getIdCode = () => {
   return url;
 };
 
-ToastHandler.showToast(`Connectting To ${url} v1.2`, true);
+ToastHandler.showToast(`Connectting To ${url} v1.3`, true);
 
 export default url;
